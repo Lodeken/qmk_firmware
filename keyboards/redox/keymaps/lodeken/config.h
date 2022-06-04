1d8b8892b588041d49ba9c2e98f9b72509c1a6ed
lodeken <lodeken@hotmail.com>
2022-06-04 21:16:39 +0800

    created own map

---
+++ keyboards/redox/keymaps/lodeken/config.h
@@ -0,0 +1,79 @@
+/*
+Copyright 2018 Mattia Dal Ben <matthewdibi@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// // default but used in macros
+#undef TAPPING_TERM
+#define TAPPING_TERM 220
+
+#define TAPPING_TERM_PER_KEY
+#define TAPPING_FORCE_HOLD_PER_KEY
+
+// // Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// newly added on suggestion here https://getreuer.info/posts/keyboards/achordion/index.html#achordion_eager_mod
+#define PERMISSIVE_HOLD
+
+// Recommended for heavy chording.
+#define QMK_KEYS_PER_SCAN 4
+
+//// Mouse key speed and acceleration.
+//#undef MOUSEKEY_DELAY
+//#define MOUSEKEY_DELAY          0
+//#undef MOUSEKEY_INTERVAL
+//#define MOUSEKEY_INTERVAL       16
+//#undef MOUSEKEY_WHEEL_DELAY
+//#define MOUSEKEY_WHEEL_DELAY    0
+//#undef MOUSEKEY_MAX_SPEED
+//#define MOUSEKEY_MAX_SPEED      6
+//#undef MOUSEKEY_TIME_TO_MAX
+//#define MOUSEKEY_TIME_TO_MAX    64
+
+/* Use I2C or Serial, not both */
+#define USE_SERIAL
+// #define USE_I2C
+
+/* Select hand configuration */
+// #define MASTER_LEFT
+#define MASTER_RIGHT
+// #define EE_HANDS
+
+/*
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 14
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+*/
+
+// Reducing firmware size https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+// Safe to remove
+// #define NO_ACTION_MACRO
+// #define NO_ACTION_FUNCTION
+
+// Depends on your usecase
+// #define NO_ACTION_ONESHOT
+
+#define COMBO_TERM_PER_COMBO
+#define COMBO_TERM 40
+
+#define CAPS_WORD_IDLE_TIMEOUT 3000 // Turn off Caps Word after 3 seconds.
+
+// #define BILATERAL_COMBINATIONS 5000
